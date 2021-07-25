43254539a7deba49f484998e235a2f12271376b7
Lizaveta Kapusta <kapustaeliz@gmail.com>
2021-07-25 17:15:32 +0300

    add sort houses api test

---
+++ ApiHousesTest/HousesModel.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousesApiTest
+{
+    class HousesModel
+    {
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+        
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "region")]
+        public string Region { get; set; }
+
+        [JsonProperty(PropertyName = "coatOfArms")]
+        public string CoatOfArms { get; set; }
+
+        [JsonProperty(PropertyName = "words")]
+        public string Words { get; set; }
+        
+        [JsonProperty(PropertyName = "titles")]
+        public string[] Titles { get; set; }
+
+        [JsonProperty(PropertyName = "seats")]
+        public string[] Seats { get; set; }
+
+        [JsonProperty(PropertyName = "currentLord")]
+        public string CurrentLord { get; set; }
+        
+        [JsonProperty(PropertyName = "heir")]
+        public string Heir { get; set; }
+
+        [JsonProperty(PropertyName = "overlord")]
+        public string Overlord { get; set; }
+
+        [JsonProperty(PropertyName = "founded")]
+        public string Founded { get; set; }
+
+        [JsonProperty(PropertyName = "founder")]
+        public string Founder { get; set; }
+
+        [JsonProperty(PropertyName = "diedOut")]
+        public string DiedOut { get; set; }
+
+        [JsonProperty(PropertyName = "ancestralWeapons")]
+        public string[] AncestralWeapons { get; set; }
+
+        [JsonProperty(PropertyName = "cadetBranches")]
+        public string[] CadetBranches { get; set; }
+
+        [JsonProperty(PropertyName = "swornMembers")]
+        public string[] SwornMembers { get; set; }
+    }
+}
---
+++ ApiHousesTest/HousesTests.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+
+namespace HousesApiTest
+{
+    [AllureNUnit]
+    [AllureSuite("API")]
+    [AllureSubSuite("Role Tests")]
+    public class HousesTests
+    {
+        private const string BASE_URL = "https://www.anapioficeandfire.com/api/";
+        private JsonSchema schema = JsonSchema.Parse(
+                @"{
+            'url': {'type':'string'},
+            'name': {'type': 'string'},
+            'region': {'type':'string'},
+            'coatOfArms': {'type': 'string'},
+            'words': {'type':'string'},
+            'titles': {'type': 'array'},
+            'seats': {'type':'array'},
+            'currentLord': {'type': 'string'},
+            'overlord': {'type':'string'},
+            'founded': {'type': 'string'},
+            'founder': {'type':'string'},
+            'diedOut': {'type': 'string'},
+            'ancestralWeapons': {'type':'array'},
+            'cadetBranches': {'type': 'array'},
+            'swornMembers': {'type':'array'}
+        }");
+
+        [Test, Description("Checking houses json scheme")]
+        public void Test_SortHouses()
+        {
+            //Creating Client connection
+            RestClient client = new RestClient(BASE_URL);
+            //Creating request to get data from server
+            RestRequest request = new RestRequest("houses",Method.GET);
+           
+            //Specify query string
+            request.AddParameter("region", "Dorne");
+            request.AddParameter("haswords", "true");
+            request.AddParameter("application/json; charset=utf-8", ParameterType.RequestBody);
+            
+            // Executing request to server and checking server response to the it
+            IRestResponse response = client.Execute(request);
+            var housesModel = JsonConvert.DeserializeObject<List<HousesModel>>(response.Content);
+            
+            //JSON validation against JSON schema
+            JArray jsonArray = JArray.Parse(response.Content);
+            var data = JObject.Parse(jsonArray[0].ToString());
+            var valid = data.IsValid(schema);
+
+            Assert.Multiple(() =>
+            {
+                foreach (var val in housesModel)
+                {
+                    Assert.True(val.Region.Equals("Dorne"), "All regions aren't equal Dorne");
+                    Assert.NotNull(val.Words, "All word aren't equal null");
+                }
+                
+                Assert.True(valid,"Json Schema isn't correct");
+                Assert.That(response.ContentType, Is.EqualTo("application/json; charset=utf-8"), "ContentType isn't application/json");
+                Assert.AreNotEqual(response.StatusCode, HttpStatusCode.BadRequest, "HttpStatusCode isn't BadRequest");
+            });
+        }
+    }
+}
